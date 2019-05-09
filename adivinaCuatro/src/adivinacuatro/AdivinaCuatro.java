7615f71faf33abee51fa7dd00f2fb1fe3299b9ea
Juan Francisco <juanfmariani85@gmail.com>
2019-05-09 16:46:51 -0300

    Inicio del Proyecto

---
+++ adivinaCuatro/src/adivinacuatro/AdivinaCuatro.java
@@ -0,0 +1,23 @@
+/*
+* EJERCICIO 1:
+* El juego para adivinar un numero de cuatro cifras (que no se repiten) consiste en que el "pensador" 
+* piensa un numero de 4 cifras (como "1234" o "9072"). El "adivinador" prueba de adivinar el numero 
+* diciendo un numero de 4 cifras y el "pensador" le dice cuantas cifras están en el lugar correcto 
+* y cuantas en el lugar incorrecto
+*/
+package adivinacuatro;
+
+/**
+ *
+ * @author sanfrarow
+ */
+public class AdivinaCuatro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
