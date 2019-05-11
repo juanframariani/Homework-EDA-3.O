3e776b9c26dbf3fb0216ba7027c5fc6a028e9d1a
Juan Francisco <juanfmariani85@gmail.com>
2019-05-11 19:12:18 -0300

    Bucles FOR para comparar ambos string de números. Contador para aciertos y regulares

--- adivinaCuatro/src/adivinacuatro/AdivinaCuatro.java
+++ adivinaCuatro/src/adivinacuatro/AdivinaCuatro.java
@@ -20,15 +20,47 @@ public class AdivinaCuatro {
     public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in);
-
+        int aciertos = 0;
+        int regular = 0;
+        
         //genera un numero random y lo muestra por pantalla.
         //el largo de la cadena se puede especificar. en este caso, dejamos 4 hardcodeado
         String numeroGenerado = GenerarNumeroRandom.generarNumero(4);
         
+        //Muestra el numero generado solo para probar -- ELIMINAR DESPUES
         System.out.println(numeroGenerado);
        
-
+        System.out.println("Ingrese un numero de 4 cifras");
         
-    }
-    
+        while(true){
+            
+            String cadena = entrada.nextLine();
+            
+            //bucle for anidado para revisar los aciertos
+            for (int i = 0; i < cadena.length(); i++) {
+                for (int j = 0; j < cadena.length(); j++) {
+                    if (cadena.charAt(i) == numeroGenerado.charAt(j) && (i == j)) {
+                        aciertos++;
+                    }
+                }
+            }
+            //bucle for para revisar si el numero esta contenido pero en distinta posicion
+            for (int i = 0; i < cadena.length(); i++) {
+                if (numeroGenerado.indexOf(cadena.charAt(i)) != -1 && !(cadena.charAt(i) == numeroGenerado.charAt(i))) {
+                        regular++;
+                }
+            }
+            
+            if (cadena.equals(numeroGenerado)) {
+                System.out.println("ADIVINO");
+                break;
+            } else {
+                System.out.println("Aciertos: " + aciertos + " - Regular: " + regular);
+                System.out.println("Vuelva a intentar:");
+                aciertos = 0;
+                regular = 0;
+            } 
+        }  
+        
+    }    
 }
